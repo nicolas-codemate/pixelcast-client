903490e2a2d95c916f42dd49f81067d785f87517
Nicolas <nicolas@codemate.consulting>
2026-08-02 19:23:31 +0200

    feat(scheduler): expose an ordered sync message registry from App\Schedule

--- src/Schedule.php
+++ src/Schedule.php
@@ -10,11 +10,16 @@ use Symfony\Component\Scheduler\ScheduleProviderInterface;
 use Symfony\Contracts\Cache\CacheInterface;
 
 #[AsSchedule]
-class Schedule implements ScheduleProviderInterface
+class Schedule implements ScheduleProviderInterface, SyncMessageRegistry
 {
     public function __construct(
         private CacheInterface $cache,
     ) {
+    }
+
+    public function syncMessages(): array
+    {
+        return [];
     }
 
     public function getSchedule(): SymfonySchedule
